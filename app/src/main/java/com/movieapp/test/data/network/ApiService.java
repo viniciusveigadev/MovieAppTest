7f9cf0170f2acf1669dff42ec5e9d60da439c778
Vinicius <viniciuslimaveiga11@gmail.com>
2021-09-11 03:46:59 -0300

    build: add Moshi converter into retrofit API (GSON is also avaiable)

---
+++ app/src/main/java/com/movieapp/test/data/network/ApiService.java
@@ -0,0 +1,20 @@
+package com.movieapp.test.data.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class ApiService {
+
+    public static FilmesService INSTANCE; //Nome da sua interface que vai receber um instancia
+
+    public static FilmesService getInstance() {
+        if (INSTANCE == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(" https://api.themoviedb.org/3/")
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .build();
+            INSTANCE = retrofit.create(FilmesService.class); //Retrofit de fato esta criando uma instancia para então ser possivel acessar rotas do tipo GET etc. disponíveis na interface;
+        }
+        return INSTANCE;
+    }
+}
--- app/src/main/java/com/movieapp/test/ui/ListaFilmesActivity.java
+++ app/src/main/java/com/movieapp/test/ui/ListaFilmesActivity.java
@@ -9,9 +9,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.movieapp.test.R;
 import com.movieapp.test.data.model.Filme;
+import com.movieapp.test.data.network.ApiService;
 
 import java.util.Arrays;
 import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ListaFilmesActivity extends AppCompatActivity {
     @Override
@@ -20,6 +26,21 @@ public class ListaFilmesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_filmes);
         //1
         RecyclerView recyclerFilmes = findViewById(R.id.recycler_view);
+
+        //5
+        ApiService.getInstance().obterFilmesPopulares("fcb6188dc381c27658a694c99c2bbd12")
+                .enqueue(new Callback<ResponseBody>() { //.execute() fará a aplicação quebrar pois esta é a thread principal;
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                    }
+                });
+
         //2
         RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
         //3
@@ -27,7 +48,7 @@ public class ListaFilmesActivity extends AppCompatActivity {
         recyclerFilmes.setAdapter(new ListaFilmesAdapter(criaFilmes())); //Recebe de fato a Classe ListaFilmesAdapter que esta tudo configurado
 
     }
-
+        //4
     private List<Filme> criaFilmes() {
         return Arrays.asList(
                 new Filme("Homem aranha"),
